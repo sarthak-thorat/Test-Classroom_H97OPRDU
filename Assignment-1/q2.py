73e327f19d232f6e6641a50a85c6d071b2c56be0
sarthak-thorat <sarthak.thorat091@gmail.com>
2026-02-04 22:05:31 +0530

    Update q2.py

--- Assignment-1/q2.py
+++ Assignment-1/q2.py
@@ -1,11 +1,26 @@
-# q2.py
+def checkValue(n):
+    if n%2 == 0:
+        print("It is an even number")
+    else:
+        print("It is an odd number")
+        
+checkValue(7)
 
-"""
-Define a function that can accept an integer number as input and print the "It is an even number" if the number is even, otherwise print "It is an odd number".
+### Question 33
+Define a function which can print a dictionary where the keys are numbers between 1 and 3 (both included) and the values are square of keys.
 
 Hints:
 
-Use % operator to check if a number is even or odd.
-"""
+Use dict[key]=value pattern to put entry into a dictionary.
+Use ** operator to get power of a number.
 
-# Start coding below...
+Solution
+​```python
+def printDict():
+    d=dict()
+    d[1]=1
+    d[2]=2**2
+    d[3]=3**2
+    print(d)
+        
+printDict()
